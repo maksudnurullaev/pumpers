ed0570942f54b1b5f3fcc1388b3fd22acb34bcf0
Maksud Nurullaev <maksud.nurullaev@gmail.com>
2025-01-30 18:27:35 +0500

    Use simple macro for seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,14 +10,38 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-admin_role = Pumpers.Accounts.Role.changeset(%Pumpers.Accounts.Role{}, %{name: "Administrators", admin: true})
-user_role = Pumpers.Accounts.Role.changeset(%Pumpers.Accounts.Role{}, %{name: "Registered Users", admin: false})
-roles = [admin_role, user_role]
-Enum.each(roles, fn role ->
-  case Pumpers.Repo.insert(role) do
-    {:ok, _role} ->
-      IO.puts("#{role.changes.name} role created")
-    {:error, changeset} ->
-      IO.puts("... WARN: role '#{changeset.changes.name}' already exists!")
+defmodule SeedHelper do
+  alias Pumpers.Accounts.Role
+  alias Pumpers.Repo
+
+  defmacro check_and_insert_role(role_name, admin_status) do
+    quote do
+      require Ecto.Query
+
+      if(Role |> Ecto.Query.where(name: unquote(role_name)) |> Repo.exists?()) do
+        IO.puts("... ROLE: '#{unquote(role_name)}' already exists!")
+      else
+        role_changeset =
+          Pumpers.Accounts.Role.changeset(%Pumpers.Accounts.Role{}, %{
+            name: unquote(role_name),
+            admin: unquote(admin_status)
+          })
+
+        Pumpers.Repo.insert(role_changeset)
+        IO.puts("... ROLE: '#{unquote(role_name)}' created!")
+      end
+    end
   end
-end)
+end
+
+defmodule Seeds do
+  require SeedHelper
+
+  def go do
+    SeedHelper.check_and_insert_role("Registered Users", false)
+    SeedHelper.check_and_insert_role("Powered Users", false)
+    SeedHelper.check_and_insert_role("Administrators", true)
+  end
+end
+
+Seeds.go()
